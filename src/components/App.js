e4c360dba963a8a6aa8f25e58463d3d8777d8733
Josh Teperman <joshteperman@gmail.com>
2019-08-26 14:26:46 +1000

    displaying youtube thumbnails from search

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../api/youtube'
+import VideoList from './VideoList'
 
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = { videos: [] }
   }
 
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
+    this.setState({
+      videos: response.data.items
+    })
   }
 
   render() {
+    const { videos } = this.state
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoList videos={videos} />
       </div>
     );
   }
