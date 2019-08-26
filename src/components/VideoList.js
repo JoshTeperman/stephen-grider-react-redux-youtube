5b1c1af32b6f87d984fe65eb921b919244bcc516
Josh Teperman <joshteperman@gmail.com>
2019-08-26 16:53:00 +1000

    styling

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,10 +3,9 @@ import VideoItem from './VideoItem'
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem video={video}/>
+    return <VideoItem video={video} key={video.id.videoId}/>
   })
-  // console.log(renderedList);
-  return <div>{renderedList}</div>
+  return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
 export default VideoList;
